0a6d98d91758b71da130e3af61dcccd928906003
John Chodera <choderaj@mskcc.org>
2016-10-26 17:41:37 -0400

    More edits to match simulation conditions

--- chloroform/simulate-density.py
+++ chloroform/simulate-density.py
@@ -10,19 +10,22 @@ Compute density of liquid chloroform using OpenMM
 
 from simtk import unit, openmm
 from simtk.openmm import app
+import numpy as np
 
 ################################################################################
 # PARAMETERS
 ################################################################################
 
-temperature = 298.0 * unit.kelvin
+temperature = 298.15 * unit.kelvin
 pressure = 1.0 * unit.atmospheres
-timestep = 2.0 * unit.femtoseconds
+timestep = 1.0 * unit.femtoseconds
 collision_rate = 5.0 / unit.picoseconds
+switch_width = 0.5*unit.angstroms
 cutoff = 11.0*unit.angstroms
 constraints = app.HBonds
-nsteps = 10000
-nsteps_per_snapshot = 500
+simulation_length = 100 * unit.nanoseconds
+nsteps = int(np.ceil(simulation_length / timestep))
+nsteps_per_snapshot = 50000
 nsteps_per_density = 50
 output_pdbfile = 'output.pdb'
 density_outfile = 'statedata.out'
@@ -44,10 +47,24 @@ print('Adding barostat...')
 barostat = openmm.MonteCarloBarostat(pressure, temperature)
 system.addForce(barostat)
 
+# Ensure we are using a switching function
+forces = { system.getForce(index).__class__.__name__ : system.getForce(index) for index in range(system.getNumForces()) }
+forces['NonbondedForce'].setUseSwitchingFunction(True)
+forces['NonbondedForce'].setSwitchingDistance(cutoff - switch_width)
+
+# Ensure we are using mixed precision
+print('Ensuring mixed precision...')
+# Select platform automatically; use mixed precision
+integrator = openmm.VerletIntegrator(timestep)
+context = openmm.Context(system, integrator)
+platform = context.getPlatform()
+del context
+platform.setPropertyDefaultValue('Precision', 'mixed')
+
 # Create simulation
 print('Creating simulation...')
 integrator = openmm.LangevinIntegrator(temperature, collision_rate, timestep)
-simulation = app.Simulation(top.topology, system, integrator)
+simulation = app.Simulation(top.topology, system, integrator, platform)
 simulation.context.setPositions(pdbfile.positions)
 
 # Minimize energy
@@ -60,5 +77,5 @@ simulation.reporters.append(app.PDBReporter(output_pdbfile, nsteps_per_snapshot)
 simulation.reporters.append(app.StateDataReporter(density_outfile, nsteps_per_density, step=True, density=True))
 
 # Run the simulation
-print('Running the simulation...')
+print('Running the simulation for %d steps...' % nsteps)
 simulation.step(nsteps)
